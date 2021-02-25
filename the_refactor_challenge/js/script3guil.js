951fe79caa26d2edc33fd186fc000ac0b7c6bda1
Skyler Harris <skyler.iqeq@gmail.com>
2021-02-25 11:15:12 +0800

    Refactor Challenge- Duplicate Code Guil 1.0

---
+++ the_refactor_challenge/js/script3guil.js
@@ -0,0 +1,16 @@
+let html = '';
+let red;
+let green;
+let blue;
+let randomRGB;
+const randomValue = () => Math.floor(Math.random() * 256);
+
+for ( let i = 1; i <= 10; i++ ) {
+    red = randomValue();
+    green = randomValue();
+    blue = randomValue();
+    randomRGB = `rgb( ${red}, ${green}, ${blue} )`;
+    html += <div style="background-color: ${randomRGB}">${i}</div>;
+}
+
+document.querySelector('main').innerHTML = html;
